73a19f88f9bbdba61f661f73dd31954fccfbd06f
Aaron Brazier <aaronbrazier@gmail.com>
2018-11-04 17:49:46 -0600

    update

--- Video2/tweet.py
+++ Video2/tweet.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = 'ZJeKLEf370VV2MWfqPfiqFlnl'
-consumer_secret = 'HMEs2B9sOw8zMtYY7PnjfHvxo7YK3aQTZ0Jqxn7RXHZu63JQbl'
+consumer_key = ''
+consumer_secret = ''
 
-access_token = '38060521-I2Ves38viCdoY7CWHhWwrIyjZkUiGOSpPFvulbJ6M'
-access_token_secret = 'vhJC7SYPJpjYRNpMqRfQNCrrlem4dmTxakniMl9zevz2N'
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)

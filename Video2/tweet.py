0c79644e08e5119b8e944e4c040021700a87479c
Aaron Brazier <aaronbrazier@gmail.com>
2018-11-04 17:41:13 -0600

    video 2 files

---
+++ Video2/tweet.py
@@ -0,0 +1,20 @@
+import tweepy
+from textblob import TextBlob
+
+consumer_key = 'ZJeKLEf370VV2MWfqPfiqFlnl'
+consumer_secret = 'HMEs2B9sOw8zMtYY7PnjfHvxo7YK3aQTZ0Jqxn7RXHZu63JQbl'
+
+access_token = '38060521-I2Ves38viCdoY7CWHhWwrIyjZkUiGOSpPFvulbJ6M'
+access_token_secret = 'vhJC7SYPJpjYRNpMqRfQNCrrlem4dmTxakniMl9zevz2N'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('Donald Trump')
+
+for tweet in public_tweets:
+    print(tweet.text)
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
